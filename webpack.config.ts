de9d96a228ccc27654e24113d69f5177b96329ae
shkumagai <take.this.2.your.grave@gmail.com>
2017-10-04 14:43:21 +0900

    Fix typo

--- webpack.config.ts
+++ webpack.config.ts
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   entry: {
-    index: path.join(__dirname + '/src/index.ts')
+    index: path.join(__dirname, '/src/index.ts')
   },
   output: {
     path: path.join(__dirname, '/build/'),
